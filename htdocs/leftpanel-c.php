fb9c6b78fb60624a69f3a3da3398c56cbcbf0436
Charles Roth <croth@thedance.net>
2026-05-29 13:48:09 -0400

    Start work on candidates editor

---
+++ htdocs/leftpanel-c.php
@@ -0,0 +1,190 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CharlesRothDotNet\EditorV4;
+
+use CharlesRothDotNet\Alfred\Str;
+use CharlesRothDotNet\Alfred\SmartyPage;
+use CharlesRothDotNet\Alfred\EnvFile;
+use CharlesRothDotNet\Alfred\PdoHelper;
+use CharlesRothDotNet\Alfred\DumbFileLogger;
+use CharlesRothDotNet\Alfred\CookieBoss;
+use CharlesRothDotNet\Alfred\CookieVerifier;
+use CharlesRothDotNet\EditorV4\EnvHelper;
+
+require_once('../vendor/autoload.php');
+
+date_default_timezone_set("America/New_York");
+
+$env              = new EnvFile("_env");
+$logger           = new DumbFileLogger($env->get('logFile'));
+$email            = EnvHelper::getEmail($env);
+$editableCounties = EnvHelper::getEditableCounties($env);
+$pdo              = PdoHelper::makePdo($env);
+
+$sql = "SELECT readCounties FROM azure_users WHERE email='$email'";
+$result = $pdo->run($sql);
+$readableCounties = $result->getSingleValue('readCounties');
+
+$allowedCounties = getUnion($editableCounties, $readableCounties);
+$allowedState    = Str::contains($allowedCounties, "999");
+$allowedCountyNums = Str::split($allowedCounties, ",");
+
+$sql = "   SELECT 'us' AS org, " . calculateTopSeats("'us', 'us-sen', 'us-hou'")
+     . "UNION "
+     . "   SELECT 'mi' AS org, " . calculateTopSeats("'mi', 'mi-sos', 'mi-ag', 'crt-sup'")
+     . "UNION "
+     . "   SELECT 'mi_sen' AS org, " . calculateTopSeats("'mi-sen'")
+     . "UNION "
+     . "   SELECT 'mi_hou' AS org, " . calculateTopSeats("'mi-hou'")
+     . "UNION "
+     . "   SELECT 'mi_boe' AS org, " . calculateTopSeats("'mi-boe','mi-msu','mi-um','mi-wsu'")
+;
+$result = $pdo->run($sql);
+$topOffices = [];
+foreach ($result->getRows() as $row)   $topOffices[$row['org']] = [$row['seats']];
+
+$counties = [];
+$allowedCountyNums = [81, 82];
+foreach ($allowedCountyNums as $countyNum) {
+
+   $sql = "   SELECT 'cnty' AS org, id, name, 1 AS link, " .  calculateSeats("'cnty', 'cnty-com'", "c.id")
+        . "     FROM s4counties AS c  WHERE id = $countyNum "
+        . "UNION "
+        . "   SELECT 'city' AS org, j.id, j.name, IF(c.id IS NULL, 0, 1) AS link, " . calculateSeats("'city', 'city-cou'", "j.id")
+        . "     FROM      s4jurisdictions AS j "
+        . "     LEFT JOIN v4completed     AS c  ON (c.id = j.id  AND c.type='city') "
+        . "    WHERE j.type='c'  AND  j.county_id = $countyNum "
+        . "UNION "
+        . "   SELECT 'town' AS org, j.id, j.name, 1 AS link , "  .  calculateSeats("'town', 'town-cou'", "j.id")
+        . "     FROM      s4jurisdictions AS j "
+        . "    WHERE j.type='t'  AND  j.county_id = $countyNum "
+        . "UNION "
+        . "   SELECT 'vil' AS org, v.id, v.name, IF(c.id IS NULL, 0, 1) AS link,"  .  calculateSeats("'vil-cou'", "v.id")
+        . "     FROM      s4villages  AS v "
+        . "     LEFT JOIN v4completed AS c  ON (c.id = v.id  AND c.type='village') "
+        . "    WHERE v.county_id = $countyNum "
+        . "UNION "
+        . "   SELECT 'schl-cou' AS org, s.id, s.name, IF(c.id IS NULL, 0, 1) AS link , "  . calculateSeats("'schl-cou'", "s.id")
+        . "     FROM      s4schools   AS s "
+        . "     LEFT JOIN v4completed AS c  ON (c.id = s.id  AND c.type='school') "
+        . "    WHERE s.county_id = $countyNum "
+        . "UNION "
+        . "   SELECT 'comcol-cou' AS org, m.id, m.name, IF(c.id IS NULL, 0, 1) AS link, " . calculateSeats("'comcol-cou'", "m.id")
+        . "     FROM      s4commcolleges        AS m "
+        . "     LEFT JOIN v4commcolleges_county AS y  ON (m.id = y.id) "
+        . "     LEFT JOIN v4completed           AS c  ON (c.id = m.id  AND c.type='college') "
+        . "    WHERE y.county_id = $countyNum "
+        . "UNION "
+        . "   SELECT type AS org, shortname AS id, name, 1 AS link, "
+        . "      (SELECT COUNT(*) FROM v4seats WHERE org=type AND district=shortname) AS seats "
+        . "    FROM  v4courts "
+        . "    WHERE county_id = $countyNum "
+        . "ORDER BY FIELD (org, 'city', 'town', 'vil', 'schl-cou', 'comcol-cou', 'crt-a', 'crt-c', 'crt-d', 'crt-pd', 'crt-p', 'crt-m'), name ";
+
+// if ($countyNum === 81) $logger->log("Big: $sql");
+
+   $result = $pdo->run($sql);
+   if ($result->failed()) $logger->log("Failed: leftpanel main select: " . $result->getError() . "  $sql");
+   foreach ($result->getRows() as $row) {
+      $org = $row['org'];
+      $name = simplifyName($row['name']);
+      $district = $row['id'];
+      $link     = intval($row['link']);
+      $seats    = intval($row['seats']);
+      switch ($org) {
+         case 'cnty':
+            $name = Str::replaceAll($name, " County", "");
+            $counties[$countyNum] = ['cnty' => [$org, $district, $name, 1, $seats],
+               'city' => [], 'town' => [], 'vil' => [], 'schl' => [], 'crt' => [], 'comcol' => [],
+               'city_num' => 0, 'city_den' => 0,
+               'town_num' => 0, 'town_den' => 0,
+               'vil_num'  => 0, 'vil_den'  => 0,
+               'schl_num' => 0, 'schl_den' => 0,
+               'col_num'  => 0, 'col_den'  => 0,
+               'crt_num'  => 0, 'crt_den'  => 0,
+               'grd_num'  => $seats,
+               'grd_den'  => $seats
+            ];
+            break;
+
+         case 'city':
+            $counties[$countyNum]['city'][] = [$org, $district, $name, $link, $seats];
+            rollUp($counties[$countyNum], 'city', $seats);
+            break;
+
+         case 'town':
+            $counties[$countyNum]['town'][] = [$org, $district, $name, $link, $seats];
+            rollUp($counties[$countyNum], 'town', $seats);
+            break;
+
+         case 'vil':
+            $counties[$countyNum]['vil']  [] = [$org, $district, $name, $link, $seats];
+            rollUp($counties[$countyNum], 'vil', $seats);
+            break;
+
+         case 'schl-cou':
+            $counties[$countyNum]['schl'] [] = [$org, $district, $name, $link, $seats];
+            rollUp($counties[$countyNum], 'schl', $seats);
+            break;
+
+         case 'comcol-cou':
+            $counties[$countyNum]['comcol'] [] = [$org, $district, $name, $link, $seats];
+            rollUp($counties[$countyNum], 'col', $seats);
+            break;
+
+         case 'crt-a':
+         case 'crt-c':
+         case 'crt-d':
+         case 'crt-pd':
+         case 'crt-p':
+         case 'crt-m':
+            $counties[$countyNum]['crt'] [] = [$org, $district, $name, $org, $seats];
+            rollUp($counties[$countyNum], 'crt', $seats);
+            break;
+      }
+   }
+}
+
+$smarty = new SmartyPage();
+$smarty->assign('allowedState', $allowedState);
+$smarty->assign('counties', $counties);
+$smarty->assign('topOffices', $topOffices);
+$smarty->display('leftpanel-c.tpl');
+
+function rollUp(array &$county, string $org, int $seats): void {
+   $county["{$org}_num"] += $seats;
+   $county['grd_num']    += $seats;
+   $county["{$org}_den"] += max(1, $seats);
+   $county['grd_den']    += max(1, $seats);   // if we have zero seats, pretend we have at least one for roll-up purposes.
+}
+
+function calculateSeats (string $orgs, string $districtField): string {
+   return "(SELECT COUNT(*) FROM v4seats WHERE org IN ($orgs) AND district=$districtField) AS seats ";
+}
+
+function simplifyName(string $text): string {
+   $name = ucwords(strtolower($text));
+   if (Str::contains($name, " Community "))       $name = Str::replaceFirst($name, " Community ", " Comm. ");
+   if (Str::endsWith($name, " Schools"))          $name = Str::replaceFirst($name, " Schools",    "");
+   if (Str::endsWith($name, " School District"))  $name = Str::replaceFirst($name, " School District",    "");
+   return $name;
+}
+
+function showArray (array $aa): string {
+   $keyValues = [];
+   foreach ($aa as $key => $value) $keyValues[] = "$key=>$value";
+   return "[" . Str::join($keyValues, ", ") . "]";
+}
+
+function getUnion (string $counties1, string $counties2): string {
+   $c1 = Str::split($counties1, ",");
+   $c2 = Str::split($counties2, ",");
+   $union = array_unique(array_merge($c1, $c2));
+   return Str::join($union, ",");
+}
+
+function calculateTopSeats (string $orgs): string {
+   return " (SELECT COUNT(*) FROM v4seats WHERE org IN ($orgs)) AS seats ";
+}
---
+++ templates/leftpanel-c.tpl
@@ -0,0 +1,176 @@
+{nocache}
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto">
+   <style>
+      body, td, li {
+        font-family: 'Roboto';
+        font-size: 0.8rem;
+      }
+      ul {
+         list-style: none;
+         padding-left: 2.0em;
+      }
+      .parent {
+         text-decoration: none;
+         color: inherit;
+      }
+      .child {
+         color: inherit;
+      }
+      .incomplete {
+         color: red;
+      }
+      .arrow {
+         font-size: 80%;
+         display: inline-block;
+         width: 0.9em;
+      }
+      .green {
+          color: darkgreen;
+      }
+   </style>
+   <script>
+      function flipArrow(idBase) {
+         let arrow = document.getElementById("A" + idBase);  // The right/down arrow
+         let ul    = document.getElementById("C" + idBase);  // the <ul> underneath it
+         if (arrow.innerHTML.charCodeAt(0) == 9654) { arrow.innerHTML = '&#9660;';   ul.style.display = "block"; }
+         else                                       { arrow.innerHTML = '&#9654;';   ul.style.display = "none";  }
+         return false;
+      }
+
+      function loadOfficials(county, orgs, district='', show='', link=1) {
+         let officialsFrame = window.parent.frames[1];
+         if (officialsFrame.document != null) {
+             let fc = officialsFrame.document.getElementById("fieldsChanged");
+             if (fc != null  &&  fc.value != ""  &&
+                 ! confirm("There are un-saved changes!\n\nPress OK if you're sure you want to proceed.\nPress Cancel to stay on the same page.")) return;
+         }
+         officialsFrame.location.href = "candidates.php?county="  + county + "&orgs=" + orgs + "&district=" + district + "&show=" + show;
+         return false;
+      }
+
+      function forceReload() {
+          document.body.innerHTML = "<p>&nbsp;</p>&nbsp;&nbsp;<i>(...working...)</i>";
+          reloader = window.location.pathname + "?" + new Date().getTime();
+          window.location.href = reloader;
+      }
+   </script>
+</head>
+
+<body style='max-width: 65em; background-color: #CCE6FF;'>
+<button style="position: fixed; top: 10px; right: 20px;" onClick="forceReload();">Refresh</button>
+
+<ul style="padding-left: 0;">
+   {if $allowedState }
+      <li>
+          <a href="#" onClick="return loadOfficials(999, 'us,us-vp,us-sen,us-hou',        '', 'ds');" class="child">US</a>
+            ({$topOffices.us[0]})
+      </li>
+      <li>
+          <a href="#" onClick="return loadOfficials(999, 'mi,mi-lt,mi-sos,mi-ag,crt-sup', '', 's');" class="child">MI</a>
+            ({$topOffices.mi[0]})
+      </li>
+      <li>
+          <a href="#" onClick="return loadOfficials(999, 'mi-sen', '', 'd');" class="child">MI Senate</a>
+            ({$topOffices.mi_sen[0]})
+      </li>
+      <li>
+          <a href="#" onClick="return loadOfficials(999, 'mi-hou', '', 'd');" class="child">MI House</a>
+            ({$topOffices.mi_hou[0]})
+      </li>
+      <li>
+         <a href="#" onClick="return loadOfficials(999, 'mi-boe,mi-msu,mi-um,mi-wsu', '', 's');" class="child">MI Education</a>
+           ({$topOffices.mi_boe[0]})
+      </li>
+   {/if}
+
+   {foreach from=$counties item=county}
+      <li><a href='#' class="parent" onClick="return flipArrow('{$county.cnty[1]}');"
+           ><span id='A{$county.cnty[1]}' class="arrow">&#9654;</span> {$county.cnty[2]}</a>
+          ({$county.grd_num}/{$county.grd_den})
+         <ul id="C{$county.cnty[1]}" style="display: none;">
+            <li><span class="arrow">&nbsp;</span>
+                <a href="#" onClick="return loadOfficials({$county.cnty[1]}, 'cnty,cnty-com', '{$county.cnty[1]}', 'w');" class="child">County Offices</a>
+                ({$county.cnty[4]})
+            </li>
+
+            <li><a href='#' class="parent" onClick="return flipArrow('{$county.cnty[1]}Y');"
+                   ><span id='A{$county.cnty[1]}Y' class="arrow">&#9654;</span> Cities</a>
+                   ({$county.city_num}/{$county.city_den})
+               <ul id="C{$county.cnty[1]}Y" style="display: none;">
+                  {foreach from=$county.city item=city}
+                     <li>
+                        <a href="#" onClick="return loadOfficials({$county.cnty[1]}, 'city,city-cou', '{$city[1]}', 'ws', 1);" class="child"     >{$city[2]}</a>
+                        ({$city[4]})
+                     </li>
+                  {/foreach}
+               </ul>
+            </li>
+
+            <li><a href='#' class="parent" onClick="return flipArrow('{$county.cnty[1]}P');"
+                ><span id='A{$county.cnty[1]}P' class="arrow">&#9654;</span> Townships</a>
+                ({$county.town_num}/{$county.town_den})
+                <ul id="C{$county.cnty[1]}P" style="display: none;">
+                    {foreach from=$county.town item=town}
+                        <li>
+                           <a href="#" onClick="return loadOfficials({$county.cnty[1]}, 'town,town-cou', '{$town[1]}', 'ws', 1);" class="child"     >{$town[2]}</a>
+                           ({$town[4]})
+                        </li>
+                    {/foreach}
+                </ul>
+            </li>
+
+             {if $county.vil|count > 0 }
+               <li><a href='#' class="parent" onClick="return flipArrow('{$county.cnty[1]}V');"
+                     ><span id='A{$county.cnty[1]}V' class="arrow">&#9654;</span> Villages</a>
+                     ({$county.vil_num}/{$county.vil_den})
+                  <ul id="C{$county.cnty[1]}V" style="display: none";>
+                     {foreach from=$county.vil item=vil}
+                         <li>
+                            <a href="#" onClick="return loadOfficials({$county.cnty[1]}, 'vil,vil-cou', '{$vil[1]}', 's', 1);" class="child"     >{$vil[2]}</a>
+                            ({$vil[4]})
+                         </li>
+                     {/foreach}
+                  </ul>
+               </li>
+            {/if}
+
+            <li><a href='#' class="parent" onClick="return flipArrow('{$county.cnty[1]}S');"
+                   ><span id='A{$county.cnty[1]}S' class="arrow">&#9654;</span> School Districts</a>
+                   ({$county.schl_num}/{$county.schl_den})
+               <ul id="C{$county.cnty[1]}S" style="display: none";>
+                  {foreach from=$county.schl item=schl}
+                      <li>
+                         <a href="#" onClick="return loadOfficials({$county.cnty[1]}, 'schl-cou', '{$schl[1]}', 's', 1);" class="child"     >{$schl[2]}</a>
+                         ({$schl[4]})
+                      </li>
+                  {/foreach}
+               </ul>
+            </li>
+
+            <li><a href='#' class="parent" onClick="return flipArrow('{$county.cnty[1]}T');"
+               ><span id='A{$county.cnty[1]}T' class="arrow">&#9654;</span> Courts</a>
+               ({$county.crt_num}/{$county.crt_den})
+               <ul id="C{$county.cnty[1]}T" style="display: none";>
+                  {foreach from=$county.crt item=crt}
+                     <li>
+                        <a href="#" onClick="return loadOfficials({$county.cnty[1]}, '{$crt[3]}', '{$crt[1]}', 's');" class="child">{$crt[2]}</a>
+                         ({$crt[4]})
+                     </li>
+                  {/foreach}
+               </ul>
+            </li>
+         </ul>
+      </li>
+   {/foreach}
+</ul>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+
+</body>
+</html>
+{/nocache}

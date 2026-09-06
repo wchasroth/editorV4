3f73868eb98ad5a62de14dba383d0593b3f12c07
Charles Roth <croth@thedance.net>
2026-09-06 10:55:47 -0400

    Remove 'us' from county report

--- htdocs/leftpanel-c.php
+++ htdocs/leftpanel-c.php
@@ -236,8 +236,7 @@ function calculateSeats (string $orgClause, string $districtField): string {
 }
 
 function calculateMetric(string $metricName, string $orgClause, string $districtField): string {
-#  $endorsedHack = ($metricName="endorsed" ? " AND c.reviewed=1 " : "");
-   $endorsedHack = "";
+   $endorsedHack = ($metricName="endorsed" ? " AND c.reviewed=1 " : "");
    return "(SELECT SUM(counter.metric) AS total_metric "
       . "   FROM ( SELECT MAX(c.$metricName) AS metric "
       . "            FROM      v4seats      AS s "

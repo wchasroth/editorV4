486499adc960b90109d50f0ed43b86fb0d3f0b26
Charles Roth <croth@thedance.net>
2026-09-16 11:10:54 -0400

    Include county office reviewed/endorsed counts in roll-up for entire county.  Sigh.

--- htdocs/leftpanel-c.php
+++ htdocs/leftpanel-c.php
@@ -168,8 +168,8 @@ foreach ($allowedCountyNums as $countyNum) {
                'schl_end' => 0, 'schl_rev' => 0, 'schl_den' => 0,
                'col_end'  => 0, 'col_rev'  => 0, 'col_den'  => 0,
                'crt_end'  => 0, 'crt_rev'  => 0, 'crt_den'  => 0,
-               'grd_end'  => 0,
-               'grd_rev'  => 0,
+               'grd_end'  => $ecount,
+               'grd_rev'  => $rcount,
                'grd_den'  => $seats
             ];
             break;

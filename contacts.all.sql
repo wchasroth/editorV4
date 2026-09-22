d2f50667a18996972abaf795af8789fa66240ce9
Charles Roth <croth@thedance.net>
2026-09-22 14:57:55 -0400

    Add generate-contact-list SQL script

---
+++ contacts.all.sql
@@ -0,0 +1,31 @@
+
+SELECT 'Filings' AS type, org, office, district, subdist, name, phone, email
+  FROM v4filings
+ WHERE party='D'
+   AND (phone!='' OR email!='')
+
+UNION ALL
+
+SELECT 'Candidates' AS type, s.org, s.office, s.district, s.subdist, c.name, c.phone, c.email
+  FROM      v4seats      AS s
+  LEFT JOIN v4candidates AS c  ON (c.seat_id = s.id)
+ WHERE c.party = 'D'
+   AND c.name  != ''
+   AND c.name  IS NOT NULL
+   AND (c.phone!='' AND c.email!='')
+
+UNION ALL
+
+SELECT 'Incumbents' AS type, s.org, s.office, s.district, s.subdist, c.name, c.phone, c.email
+  FROM      v4seats      AS s
+  LEFT JOIN v4incumbents AS c  ON (c.seat_id = s.id)
+ WHERE c.party = 'D'
+   AND c.name  != ''
+   AND c.name  IS NOT NULL
+   AND (c.phone!='' AND c.email!='')
+
+ORDER BY type, name, org, office, district, subdist
+
+   
+
+   

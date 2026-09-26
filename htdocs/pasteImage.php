9c600048eceb04c70bb15c832d124221ea6a466a
Charles Roth <wchasroth@gmail.com>
2026-09-26 12:03:41 -0400

    @

--- htdocs/pasteImage.php
+++ htdocs/pasteImage.php
@@ -6,6 +6,7 @@ namespace CharlesRothDotNet\EditorV4;
 use CharlesRothDotNet\Alfred\EnvFile;
 use CharlesRothDotNet\Alfred\NameSimplifier;
 use CharlesRothDotNet\Alfred\DumbFileLogger;
+use CharlesRothDotNet\Alfred\HttpGet;
 
 require_once('../vendor/autoload.php');
 
@@ -13,16 +14,13 @@ $env       = new EnvFile("_env");
 $logger    = new DumbFileLogger($env->get('logFile'));
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['pasted_image'])) {
-    
-    $rawData  = $_POST['pasted_image'];
     $canId    = HttpGet::number('can_id');
     $name     = $_GET['name']   ?? '';
+    $rawData  = $_POST['pasted_image'];
     $name     = NameSimplifier::makeFilenameFrom($name);
-    $logger->log("name=$name");
 
     // Validate that it is a proper base64 data URI image
-    if (preg_match('/^data:image\/(png|jpeg|jpg);base64,/', $rawData, $matches)) {
-       $logger->log("inside preg_match");
+    if (preg_match('/^data:image\/(png|jpeg|jpg|webp);base64,/', $rawData, $matches)) {
         
         $extension = $matches[1]; // Get extension (e.g., png)
         
@@ -35,12 +33,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['pasted_image'])) {
         // Save the file with a unique name in your directory
         $fileName = $canId . "-" . $name . "-pasted." . $extension;
         
+        error_clear_last();
         if (file_put_contents("PHOTOS_CAN/$fileName", $decodedData)) {
            $logger->log("success");
             echo json_encode(['success' => true, 'file' => $fileName]);
         } else {
-           $logger->log("could not save");
-            echo json_encode(['success' => false, 'error' => 'Could not save file.']);
+           $error = error_get_last();
+           $logger->log("could not save: " . $error['message'] ?? 'none');
+           echo json_encode(['success' => false, 'error' => 'Could not save file.']);
         }
     } else {
         echo json_encode(['success' => false, 'error' => 'Invalid image format.']);
